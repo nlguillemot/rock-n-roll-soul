7dacd24ac573de979fb59ab97351ff60a871b261
Nicolas Guillemot <nlguillemot@gmail.com>
2013-01-25 21:11:33 -0800

    added position/velocity/acceleration stuff

--- include/player.hpp
+++ include/player.hpp
@@ -9,13 +9,27 @@ namespace heart
 class Player
 {
 public:
+    enum State
+    {
+        Idle,
+        Jumping,
+        Falling
+    };
+
     Player();
     ~Player();
 
+    void update(sf::Uint32 dt);
     void draw(sf::RenderTarget& target);
 private:
     Animation* animation_;
     AnimData* anim_data_;
+
+    State state_;
+    
+    sf::Vector2f position_;
+    sf::Vector2f velocity_;
+    sf::Vector2f acceleration_;
 };
 
 }
--- src/player.cpp
+++ src/player.cpp
@@ -7,6 +7,7 @@ Player::Player()
 {
     anim_data_ = new AnimData("assets/bird");
     animation_ = new Animation(*anim_data_);
+    state_ = Idle;
 }
 
 Player::~Player()
@@ -15,8 +16,28 @@ Player::~Player()
     delete animation_;
 }
 
+void Player::update(sf::Uint32 dt)
+{
+    if (state_ == Idle)
+    {
+    }
+    else if (state_ == Jumping)
+    {
+    }
+    else if (state_ == Falling)
+    {
+    }
+    else
+    {
+    }
+
+    velocity_ = (dt/1000.0f) * acceleration_;
+    position_ = (dt/1000.0f) * velocity_;
+}
+
 void Player::draw(sf::RenderTarget& target)
 {
+    animation_->set_position(position_);
     animation_->draw(target);
 }
 
